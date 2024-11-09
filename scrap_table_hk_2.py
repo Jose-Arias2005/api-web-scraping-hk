cea72086e4353359021d25ba985a61f268b4e0ec
José Armando Arias Romero <166131122+Jose-Arias2005@users.noreply.github.com>
2024-11-09 11:02:25 -0500

    Update and rename scrap_table_hk.py to scrap_table_hk_2.py

--- scrap_table_hk_2.py
+++ scrap_table_hk_2.py
@@ -37,7 +37,7 @@ def lambda_handler(event, context):
 
     # Guardar los datos en DynamoDB
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('TablaWebScrappingHK')
+    table = dynamodb.Table('TablaWebScrappingHK2')
 
     # Eliminar todos los elementos de la tabla antes de agregar los nuevos
     scan = table.scan()
